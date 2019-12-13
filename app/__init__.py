5bf3f63066db24bbea0e8764119b16410dc8cade
BradOdera <bradodera@gmail.com>
2019-12-13 04:48:28 +0300

    Fix importation errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import config_options
+from .config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -11,7 +11,6 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 mail = Mail()
-
 
 def create_app(config_name):
 
@@ -27,11 +26,14 @@ def create_app(config_name):
     mail.init_app(app)
     login_manager.init_app(app)
 
+
+
+
     # Registering the blueprint
-    from app.main import main as main_blueprint
+    from app import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from app.auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+    from app import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     return app
