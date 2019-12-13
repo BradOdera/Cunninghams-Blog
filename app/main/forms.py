2592e4c3f99a656b7b577993f483352cbf3a4cab
BradOdera <bradodera@gmail.com>
2019-12-13 03:40:04 +0300

    Add forms for commenting and blogging

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,8 +3,16 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
 
-class ReviewForm(FlaskForm):
+class BlogForm(FlaskForm):
+    """docstring for PitchForm."""
 
-    title = StringField('Review title', validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
-    submit = SubmitField('Submit')
+    title = StringField('Blog title', validators=[Required()])
+    text = TextAreaField('Content', validators=[Required()])
+    submit = SubmitField('Publish')
+
+
+class CommentForm(FlaskForm):
+    """docstring for CommentForm."""
+
+    content = TextAreaField('Leave a Comment:', validators=[Required()])
+    submit = SubmitField('Post')
