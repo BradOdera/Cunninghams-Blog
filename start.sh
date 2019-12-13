fd7f9f0aed0d7100418998bc871157dfa27ef569
BradOdera <bradodera@gmail.com>
2019-12-13 16:14:24 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='rfuywelfhujufylbhsdjlhfrsrbsdhsdb'
 
-export MAIL_USERNAME=baronwilbrone@gmail.com
-export MAIL_PASSWORD=Sahara10322
+export MAIL_USERNAME=bradoera@gmail.com
+export MAIL_PASSWORD=Kingisth3m2
 
 python3.6 manage.py server
