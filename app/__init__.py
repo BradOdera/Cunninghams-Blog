f350d6d297a23c509df08248c1bf5c4a7452b0bd
BradOdera <bradodera@gmail.com>
2019-12-13 12:12:02 +0300

    Importation error

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ def create_app(config_name):
     login_manager.init_app(app)
 
     # Registering the blueprint
-    from app.main import main as main_blueprint
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
